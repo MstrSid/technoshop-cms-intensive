4ed597be0be6777351849e4b4a3b495997068a85
Eugene Kos <mstrsid@live.com>
2022-08-13 19:38:48 +0300

    done

--- technoshop-cms/script/tools.js
+++ technoshop-cms/script/tools.js
@@ -13,3 +13,7 @@ export const toBase64 = (file) => {
 		});
 	})
 }
+
+localStorage.setItem('cartGoods',
+	JSON.stringify([{id: '1234', count: 5}, {id: '1235', count: 2}]));
+console.log(JSON.parse(localStorage.getItem('cartGoods')));
